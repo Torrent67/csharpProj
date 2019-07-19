ac6eab8fe4e13f14b564c5537126005ac88c5c72
Marqis Ramos <atair01@yahoo.com>
2019-07-19 16:27:25 -0700

    the console will ask user if they want loaf or pastry, and then ask them to input ammount of loaf or pastry they desire, and then give them total LOAF or pastry and price

---
+++ Bakery/Shop.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Bakery.Shop
+ {
+
+  class Bread
+  {
+    private int _Price;
+
+    public Bread(int price)
+    {
+      _Price = price;
+    }
+
+    public int GetPrice()
+    {
+      return _Price;
+    }
+
+
+    }
+
+  
+
+}
--- Program.cs
+++ Program.cs
@@ -1,26 +1,55 @@
 using System;
 using System.Collections.Generic;
-using Bakery.Main;
+using Bakery.Shop;
 
-namespace Bakery
-{ 
-   public class Program
+namespace Bakery 
+{
+
+  public class Program
   {
-    public static void Loafs()
-    {
-      string loafs = ++i;
-      int loafP = 8;
-    }
+    
     public static void Main()
     {
-      Bread loaf = new Bread("Bread Loaf", 5);
 
-      Console.WriteLine("Welcome to Pierre's Bakery! Our bread loafs are 8 dollars each and our Pastries are 5 dollars each, please enter either Loafs or Pastries and then your desired ammount.");
-      string stringMaxPrice = Console.ReadLine();
-      int maxPrice = int.Parse(stringMaxPrice);
+      Bread Loaf = new Bread(5);
+      Bread Pastry = new Bread(2);
+     
 
+      List<Bread> Breads = new List<Bread>() { Loaf, Pastry };
+      Console.WriteLine("Welcome to Pierre's Bakery! The French loaf specialist. We have a great deal this weekend! Buy 2 loaves of our bread get 1 free. A single loaf costs $5, OR our delcious Pasties are 1 for $2 or 3 for $5.");
+      Console.WriteLine("____________________________________________________");
+      Console.WriteLine("Due to our limited stock the maxium number of loaves you may buy are 6.");
+      Console.WriteLine("____________________________________________________");
+      Console.WriteLine("Enter what you would like to purchace[Loaf] or [Pastry]");
+      string userInput = Console.ReadLine().ToLower();
+      if (userInput == "loaf")
+      {
+       BreadCost();
       }
+      else if (userInput == "pastry")
+      {
+        PastryCost();
+      }
+      
     }
+      public static void BreadCost()
+      {
+       Console.WriteLine("Enter how many loaves you would like.");
+        int userInt = int.Parse(Console.ReadLine());
+        int checkoutLoaf = (userInt /2) + userInt;
+        Console.WriteLine("Ammount of bread you're getting! " + checkoutLoaf + " And your total " + (userInt * 5) );
+      }
+
+      public static void PastryCost()
+      {
+        Console.WriteLine("Enter how many pastries you would like.");
+        int userInt = int.Parse(Console.ReadLine());
+        int checkoutPastry = (userInt /3);
+        Console.WriteLine("Ammount of pastries you're getting! " + userInt + " And your total " + (userInt * 2 - checkoutPastry) );
+      }
+
   }
 
 }
+  
+
